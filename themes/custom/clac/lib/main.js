990f7b82fd7c64044507c278eecc8b108a9f57cd
ppajal <pajalic68@gmail.com>
2017-07-24 22:10:06 +1000

    bootstarp navbar working

--- themes/custom/clac/lib/main.js
+++ themes/custom/clac/lib/main.js
@@ -1,7 +1,5 @@
 jQuery(document).ready(function($) {
 
-    $(document).foundation();
-    
     $("#block-clac-main-menu-menu, li").hover(function(){
         $(this).find(".sub-menu").toggle();
     });
